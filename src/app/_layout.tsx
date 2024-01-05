8324398e753aca62393e865e5c5169c2b5eb19ad
junki_fujihara <92765294+nomari4598@users.noreply.github.com>
2024-01-05 22:11:55 +0900

    update _layout.tsx作成追加

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Slot } from "expo-router";
+
+/**
+ * 使用媒体の画面に対して画面描画させるためのコンポーネント
+ * ※exportの仕方や書き方はこのファイルに限ってはこの通りに記載し、変更しないこと
+ *
+ * @returns
+ */
+const Layout = (): JSX.Element => {
+    return <Slot />;
+};
+
+export default Layout;
