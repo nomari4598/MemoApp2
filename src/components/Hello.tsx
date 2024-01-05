f328bc26bac2f47d4180ef6d24eb1951876679d1
junki_fujihara <92765294+nomari4598@users.noreply.github.com>
2024-01-05 10:24:09 +0900

    update Hello.tsx スタイル設定

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 type HelloProps = {
     children: string;
@@ -9,7 +9,17 @@ export const Hello = (props: HelloProps): JSX.Element => {
 
     return (
         <View>
-            <Text>Hello {children}</Text>
+            <Text style={styles.text}>Hello {children}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    text: {
+        color: "#ffffff",
+        backgroundColor: "blue",
+        fontSize: 40,
+        fontWeight: "bold",
+        padding: 16,
+    },
+});
