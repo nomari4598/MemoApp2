0e71dfdaf1bb85db8c3eba11f02d61a153139155
junki_fujihara <92765294+nomari4598@users.noreply.github.com>
2024-01-05 22:51:41 +0900

    update 初期画面構成更新

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,51 +9,126 @@ import { Text, View, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
     return (
         <View style={styles.container}>
-            <View>
-                <View>
-                    <Text>Memo App</Text>
-                    <Text>ログアウト</Text>
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>Memo App</Text>
+                    <Text style={styles.headerRight}>ログアウト</Text>
                 </View>
             </View>
             <View>
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>
+                            買い物リスト
+                        </Text>
+                        <Text style={styles.memoListItemDate}>
+                            2023年10月1日 10:00
+                        </Text>
                     </View>
                     <Text>X</Text>
                 </View>
             </View>
             <View>
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>
+                            買い物リスト
+                        </Text>
+                        <Text style={styles.memoListItemDate}>
+                            2023年10月1日 10:00
+                        </Text>
                     </View>
                     <Text>X</Text>
                 </View>
             </View>
             <View>
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>買い物リスト</Text>
-                        <Text>2023年10月1日 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>
+                            買い物リスト
+                        </Text>
+                        <Text style={styles.memoListItemDate}>
+                            2023年10月1日 10:00
+                        </Text>
                     </View>
                     <Text>X</Text>
                 </View>
             </View>
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
         </View>
     );
 };
 
+// 高さ関連の数値はpx換算
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "#ffffff",
+    },
+    header: {
+        backgroundColor: "#467FD3",
+        height: 104,
+        justifyContent: "flex-end",
+    },
+    headerInner: {
+        alignItems: "center",
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: "bold",
+        color: "#ffffff",
+    },
+    headerRight: {
+        position: "absolute",
+        right: 16,
+        bottom: 16,
+        color: "rgba(255, 255, 255, 0.7)",
+    },
+    memoListItem: {
+        backgroundColor: "#ffffff",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: "rgba(0, 0, 0, 0.15)",
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32,
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: "#848484",
+    },
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: "#467fd3",
         justifyContent: "center",
         alignItems: "center",
+        position: "absolute",
+        right: 40,
+        bottom: 40,
+        // 以下iOS製品にしか機能しない
+        shadowColor: "#000000",
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 8 },
+        // 以下android製品にしか機能しない
+        elevation: 8,
+    },
+    circleButtonLabel: {
+        color: "#ffffff",
+        fontSize: 40,
+        lineHeight: 48,
     },
 });
 
